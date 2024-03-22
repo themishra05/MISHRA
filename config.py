a100be4f3c793e1b25c6e1cb811fb1e527faa57c
THEMADMAXPRO <157195522+THEMADMAXPRO@users.noreply.github.com>
2024-03-22 18:44:20 +0530

    Update config.py

--- config.py
+++ config.py
@@ -34,15 +34,15 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/THEMADMAXPRO/MadmaxXMusic",
+    "https://github.com/themishra05/MISHRA",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "MadmaxXMusic")
 GIT_TOKEN = getenv(
     "GIT_TOKEN", None
 )  # Fill this variable if your upstream repository is private
 
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/STATUSDAIRY2")
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/VOICEOFHEART0")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/dpz_hub_05")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/friendssquadron")
 
 # Set this to True if you want the assistant to automatically leave chats after an interval
 AUTO_LEAVING_ASSISTANT = bool(getenv("AUTO_LEAVING_ASSISTANT", False))
@@ -80,10 +80,10 @@ confirmer = {}
 
 
 START_IMG_URL = getenv(
-    "START_IMG_URL", "https://telegra.ph/file/3eaaee3b5cfbc0e42f2f3.jpg"
+    "START_IMG_URL", "https://telegra.ph/file/14e83a9cce97bcc62f36d.jpg"
 )
 PING_IMG_URL = getenv(
-    "PING_IMG_URL", "https://telegra.ph/file/8a0508262e794c96a3e48.jpg"
+    "PING_IMG_URL", "https://telegra.ph/file/ae7a6de718269dec81eca.jpg"
 )
 PLAYLIST_IMG_URL = "https://te.legra.ph/file/4ec5ae4381dffb039b4ef.jpg"
 STATS_IMG_URL = "https://te.legra.ph/file/e906c2def5afe8a9b9120.jpg"
